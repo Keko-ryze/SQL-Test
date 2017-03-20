e4505210f631445826844d11789bbafdcff3b525
keko <kevin.kott95gmail.com>
2017-03-20 12:57:44 +0100

    Edit Main.xml Basic login with main activity

--- app/src/main/java/com/example/kevin/sql_test/MainActivity.java
+++ app/src/main/java/com/example/kevin/sql_test/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.kevin.sql_test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText UsernameEt, PasswordEt;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        UsernameEt = (EditText)findViewById(R.id.username);
+        PasswordEt = (EditText)findViewById(R.id.password);
     }
 }
